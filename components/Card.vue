66a88d12d145c672a04fabf23bea6c1037bbed4e
raquelbennington <raquelbennington@gmail.com>
2021-06-07 22:46:09 -0300

    a gente vive tentando

--- components/Card.vue
+++ components/Card.vue
@@ -1,26 +1,17 @@
 <template>
   <div class="w-full md:w-1/2 lg:w-1/4 pl-5 pr-5 mb-5 lg:pl-2 lg:pr-2">
     <div
-      class="
-        bg-pink
-        rounded
-        m-h-64
-        p-2
-        border-black border border-t border-b border-r
-      "
+      class="bg-pink rounded m-h-64 p-2 border-black border border-t border-b border-r"
     >
       <figure class="mb-2 py-2">
         <img :src="photo" :alt="legend" class="h-64 ml-auto mr-auto" />
       </figure>
 
-      <div class="rounded-lg p-4 bg-white flex flex-col">
-        <div class="flex items-center">
-          <div>
-            <a href="" target="_blank"
-              ><Icon
-            /></a>
-          </div>
+      <div class="rounded-lg bg-white flex justify-between">
+        <div class="flex mx-2 sm:mx-6">
+          <a href="items-center" target="_blank"><Icon/></a>
           <p class="text-base text-gray-600 leading-none">{{ legend }}</p>
+          
         </div>
       </div>
     </div>
--- store/cards.js
+++ store/cards.js
@@ -1,8 +1,13 @@
 export const state = () => ({
     info: [
 
-        { photo: require('~/assets/beyonce.jpg'), icon: 'Stevie Wonder', legend: 'Innervisions' },
-        { photo: require('~/assets/beyonce.jpg'), icon: 'Chaka Chan', legend: 'Chaka'},
-    
+        { photo: require('~/assets/card_09.PNG'), legend: 'Lorem' },
+        { photo: require('~/assets/card_01.PNG'), legend: 'Lorem' },
+        { photo: require('~/assets/card_02.PNG'), legend: 'Lorem' },
+        { photo: require('~/assets/card_04.PNG'), legend: 'Lorem' },
+        { photo: require('~/assets/card_05.PNG'), legend: 'Lorem' },
+        { photo: require('~/assets/card_03.PNG'), legend: 'Lorem' },
+        { photo: require('~/assets/card_07.PNG'), legend: 'Lorem' },
+        { photo: require('~/assets/card_08.PNG'), legend: 'Lorem' },
     ]
 })
